fa847eb01b3af95a389680af4cb42ff134a1887c
arafat-adflixconsulting <arafathossainraj@gmail.com>
2025-08-15 02:21:34 +0600

    second problem fix

--- final.js
+++ final.js
@@ -1,14 +1,25 @@
 // Problem-01 : Train TT's Fine Calculator
 
 function totalFine(fare) {
-  const surcharge = fare * 0.2;
-  const serviceCharge = 30;
-  let fine = fare + surcharge + serviceCharge;
   if (typeof fare !== "number" || fare <= 0) {
     return "Invalid";
   } else {
+    const surcharge = fare * 0.2;
+    const serviceCharge = 30;
+    let fine = fare + surcharge + serviceCharge;
     return fine;
   }
 }
 
 // Problem-02 : Clean & Capitalize Characters
+
+function onlyCharacter(str) {
+  if (typeof str !== "string") {
+    return "Invalid";
+  } else {
+    let isTrimed = str.trim();
+    let spaceRemoved = isTrimed.replaceAll(" ", "");
+    let accept = spaceRemoved.toUpperCase();
+    return accept;
+  }
+}
